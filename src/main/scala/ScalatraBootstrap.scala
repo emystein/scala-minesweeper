cf25917120cfc02fe977bd26f77634446cabbdbe
Emiliano Menéndez <emystein@gmail.com>
2020-06-30 21:33:37 -0300

    Fix ScalatraBootstrap

--- src/main/scala/ScalatraBootstrap.scala
+++ src/main/scala/ScalatraBootstrap.scala
@@ -2,7 +2,6 @@ import ar.com.flow.minesweeper._
 import javax.servlet.ServletContext
 import org.scalatra._
 import org.slf4j.LoggerFactory
-import slick.jdbc.H2Profile.api._
 
 class ScalatraBootstrap extends LifeCycle with Persistence {
   val logger = LoggerFactory.getLogger(getClass)
@@ -12,12 +11,10 @@ class ScalatraBootstrap extends LifeCycle with Persistence {
   logger.info("Created DB connection pool")
 
   override def init(context: ServletContext) {
-    val db = Database.forDataSource(dataSource, None)
-
     // TODO: move out
-    db.run(Tables.createDatabase)
+    database.run(Tables.createDatabase)
 
-    context.mount(new MinesweeperServlet(db, swagger), "/*")
+    context.mount(new MinesweeperServlet(database, swagger), "/*")
     context.mount(new SwaggerServlet(), "/api-docs")
   }
 
