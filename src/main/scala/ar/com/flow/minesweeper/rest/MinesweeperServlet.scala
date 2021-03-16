0dc654ab6ac4c1c76e9599833a24e6b78cf534c0
Emiliano Menéndez <emystein@gmail.com>
2021-03-16 18:50:01 -0300

    Extract MinesweeperSwaggerSpecs.

--- src/main/scala/ar/com/flow/minesweeper/rest/MinesweeperServlet.scala
+++ src/main/scala/ar/com/flow/minesweeper/rest/MinesweeperServlet.scala
@@ -6,12 +6,11 @@ import org.json4s.{DefaultFormats, Formats}
 import org.scalatra._
 import org.scalatra.json.NativeJsonSupport
 import org.scalatra.swagger._
-import org.scalatra.util.MimeTypes
 import slick.jdbc.H2Profile.api._
 
 import scala.concurrent.Future
 
-class MinesweeperServlet(val db: Database, implicit val swagger: Swagger) extends SwaggerUiRoute with NativeJsonSupport with SwaggerSupport with FutureSupport with DbRoutes {
+class MinesweeperServlet(val db: Database, implicit val swagger: Swagger) extends SwaggerUiRoute with MinesweeperSwaggerSpecs with NativeJsonSupport with FutureSupport with DbRoutes {
   val gameRepository = new GameRepository(db)
 
   protected implicit def executor = scala.concurrent.ExecutionContext.Implicits.global
@@ -26,59 +25,25 @@ class MinesweeperServlet(val db: Database, implicit val swagger: Swagger) extend
     contentType = formats("json")
   }
 
-  val getGames = (apiOperation[List[GameResource]]("getGames") summary "Show all games")
-
-  get("/games", operation(getGames)) {
+  get("/games", getGamesSpec) {
     gameRepository.findAll.map(f => f.map(GameResource.from))
   }
 
-  val createGame = (apiOperation[GameResource]("createGame")
-    summary ("Create a Game")
-    consumes ("application/json")
-    produces ("application/json")
-    parameter bodyParam[NewGameRequestBody]
-    )
-
-  post("/games", operation(createGame)) {
+  post("/games", startNewGameSpec) {
     response.setStatus(201)
     val parameters = parsedBody.extract[NewGameRequestBody]
     save(Game(parameters.rows, parameters.columns, parameters.bombs))
   }
 
-  val markCell = (apiOperation[GameResource]("markCell")
-    summary ("Every subsequent request toggles a mark in a Cell in cyclic order: Flag, Question, Clear mark.")
-    produces ("application/json")
-    parameters(
-    pathParam[String]("gameId"),
-    pathParam[Int]("row"),
-    pathParam[Int]("column")
-  ))
-
-  post("/games/:gameId/cell/:row/:column/toggle-mark", operation(markCell)) {
+  post("/games/:gameId/cell/:row/:column/toggle-mark", markCellSpec) {
     persistOnCell((game, cellCoordinates) => game.toggleCellMark(cellCoordinates))
   }
 
-  val revealCell = (apiOperation[GameResource]("revealCell")
-    summary ("Reveal a Cell")
-    produces ("application/json")
-    parameters(
-    pathParam[String]("gameId"),
-    pathParam[Int]("row"),
-    pathParam[Int]("column")
-  ))
-
-  post("/games/:gameId/cell/:row/:column/reveal", operation(revealCell)) {
+  post("/games/:gameId/cell/:row/:column/reveal", revealCellSpec) {
     persistOnCell((game, cellCoordinates) => game.revealCell(cellCoordinates))
   }
 
-  val pauseResumeGame = (apiOperation[GameResource]("pauseResumeGame")
-    summary ("Pause/Resume a Game")
-    produces ("application/json")
-    parameters(
-    pathParam[String]("gameId")
-    ))
-
-  post("/games/:gameId/pause-resume", operation(pauseResumeGame)) {
+  post("/games/:gameId/pause-resume", pauseResumeGameSpec) {
     gameRepository.findById(params("gameId")).map { game =>
       save(game.togglePauseResume)
     }
---
+++ src/main/scala/ar/com/flow/minesweeper/rest/MinesweeperSwaggerSpecs.scala
@@ -0,0 +1,45 @@
+package ar.com.flow.minesweeper.rest
+
+import org.scalatra.swagger._
+
+trait MinesweeperSwaggerSpecs extends SwaggerSupport {
+  val getGamesSpec = operation((apiOperation[List[GameResource]]("getGames") summary "Show all games"))
+
+  val startNewGameSpec = operation((apiOperation[GameResource]("startNewGame")
+    summary ("Start a new Game")
+    consumes ("application/json")
+    produces ("application/json")
+    parameter bodyParam[NewGameRequestBody]
+    ))
+
+  val markCellSpec = cellMutatingOperationSpec("markCell",
+    "Every subsequent request toggles a mark in a Cell in cyclic order: Flag, Question, Clear mark.")
+
+  val revealCellSpec = cellMutatingOperationSpec("revealCell", "Reveal a Cell")
+
+  val pauseResumeGameSpec = gameMutatingOperationSpec("pauseResumeGame", "Pause/Resume a Game")
+
+  private def gameMutatingOperationSpec(operationName: String, operationSummary: String) = {
+    operation(
+      (apiOperation[GameResource](operationName)
+        summary (operationSummary)
+        produces ("application/json")
+        parameter (pathParam[String]("gameId"))
+        )
+    )
+  }
+
+  private def cellMutatingOperationSpec(operationName: String, operationSummary: String) = {
+    operation(
+      (apiOperation[GameResource](operationName)
+        summary (operationSummary)
+        produces ("application/json")
+        parameters(
+        pathParam[String]("gameId"),
+        pathParam[Int]("row"),
+        pathParam[Int]("column")
+      ))
+    )
+  }
+
+}
